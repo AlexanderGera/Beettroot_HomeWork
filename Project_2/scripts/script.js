fb0c06cf27bab813f59c3c959668cc443b4f15ed
“Alexander <gerasimenko.post@gmail.com>
2023-03-26 14:32:55 +0200

    project 2 beginning

---
+++ Project_2/scripts/script.js
@@ -0,0 +1,10 @@
+    let swiper = new Swiper(".mySwiper-welcome", {
+        direction: "vertical",
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
