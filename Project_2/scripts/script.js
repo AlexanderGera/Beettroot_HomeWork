917cdfaaa0f8c8bbecfdee3796da0fa9613b9667
“Alexander <gerasimenko.post@gmail.com>
2023-04-04 00:30:53 +0200

    without JS and map

--- Project_2/scripts/script.js
+++ Project_2/scripts/script.js
@@ -2,9 +2,27 @@
         direction: "vertical",
         slidesPerView: 1,
         spaceBetween: 30,
-        mousewheel: true,
+        // mousewheel: true,
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
         },
+    });
+
+
+    // Script for Latest News  
+    let swiperLatestNews = new Swiper(".mySwiperLatestNews", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+    
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
     });
