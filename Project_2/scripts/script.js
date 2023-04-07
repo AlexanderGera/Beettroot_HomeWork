89984c4316cda8158296af2eddeb38536ddae50f
“Alexander <gerasimenko.post@gmail.com>
2023-04-07 22:38:51 +0200

    without map

--- Project_2/scripts/script.js
+++ Project_2/scripts/script.js
@@ -14,15 +14,31 @@
     let swiperLatestNews = new Swiper(".mySwiperLatestNews", {
         slidesPerView: 3,
         spaceBetween: 30,
-    
+
 
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
 
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
     });
+
+
+
+    //script for smooth scroling to anchor links
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+
